69753e79acff5daa750e3e3964b3f943feead847
César Izurieta <cesar@caih.org>
2009-10-11 08:40:54 -0300

    Code for the bot

--- wave-email-notifications.py
+++ wave-email-notifications.py
@@ -22,23 +22,35 @@ def get_wave(context):
     return context.waves[context.wavelets.values()[0]]
 
 
-def new_participants(properties, context):
+def notify(wave, modifiedBy, message):
     wave = get_wave(context)
-    for participant in properties[events.PARTICIPANTS_ADDED]:
-        send_notification(wave, participant,
-                          'You have been added as a participant to the "%s" wave. It is available at the following url:'
-                          % wave.title)
-
-
-def notify(properties, context):
-    wave = get_wave(context)
-    for participant in wave.participants:    
-        send_notification(wave, participant,
-                          'The "%s" wave has been updated. Please visit the following url to see the changes:'
-                          % wave.title)
+    for participant in wave.participants:
+        if participant != modifiedBy:
+            send_notification(wave, participant, message)
 
 
 def send_notification(wave, participant, message):
+    query = model.ParticipantPreferences.all()
+    query.filter('participant =', path_parts[2])
+    pp = query.get()
+
+    if not pp:
+        pp = model.ParticipantPreferences(participant=participant)
+        pp.put()
+    elif not pp.notify:
+        return
+
+    query = model.ParticipantWavePreferences.all()
+    query.filter('participant =', path_parts[2])
+    query.filter('waveId =', path_parts[3])
+    pwp = query.get()
+
+    if not pwp:
+        pwp = model.ParticipantWavePreferences(participant=participant, waveId=wave.waveId)
+        pwp.put()
+    elif not pwp.notify:
+        return
+
     url = 'https://wave.google.com/wave/#restored:wave:%s' % urllib.quote(wave.waveId)
     prefs_url = '%s/prefs/%s/%s/' % (ROBOT_BASE_URL, urllib.quote(participant), urllib.quote(wave.waveId))
     subject = '[wave] %s' % wave.title
@@ -55,13 +67,43 @@ To change your notification preferences please visit:
     mail.send_mail(ROBOT_ADDRESS, participant, subject, body)
 
 
+class NotificationsRobot(robot.Robot):
+
+    def __init__(self,):
+        Robot.__init__(self, ROBOT_NAME, 
+                       image_url='%s/icon.png' % ROBOT_BASE_URL, version='1',
+                       profile_url=ROBOT_BASE_URL)
+
+        self.RegisterListener(self)
+
+    def on_wavelet_participants_changed(properties, context):
+        wave = get_wave(context)
+        for participant in properties[events.PARTICIPANTS_ADDED]:
+            send_notification(wave, participant,
+                              'You have been added as a participant to the "%s" wave. It is available at the following url:'
+                              % wave.title)
+
+    def on_blip_submitted(properties, context):
+        wave = get_wave(context)
+        modifiedBy = properties['modifiedBy']
+        notify(wave, modifiedBy,
+               'The "%s" wave has been updated by %s. Please visit the following url to see the changes:'
+               % wave.title)
+
+    def on_blip_deleted(properties, context):
+        wave = get_wave(context)
+        modifiedBy = properties['modifiedBy']
+        notify(wave, modifiedBy,
+               'The "%s" wave has been updated by %s. Please visit the following url to see the changes:'
+               % wave.title)
+
+    def on_document_changed(properties, context):
+        wave = get_wave(context)
+        modifiedBy = properties['modifiedBy']
+        notify(wave, modifiedBy,
+               'The "%s" wave has been updated by %s. Please visit the following url to see the changes:'
+               % wave.title)
+
+
 if __name__ == '__main__':
-    myRobot = robot.Robot(ROBOT_NAME, 
-                          image_url='%s/icon.png' % ROBOT_BASE_URL,
-                          version='1',
-                          profile_url=ROBOT_BASE_URL)
-
-    myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, new_participants)
-    myRobot.RegisterHandler(events.WAVELET_TIMESTAMP_CHANGED, notify)
-
-    myRobot.Run()
+    NotificationsRobot().run()
