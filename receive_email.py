a7b41eef86aebcffdb0e2f6c2a9d4f8c9903e0cd
cesar <cesar@caih.org>
2009-11-26 11:46:37 -0200

    Ability to receive unsubscribe emails

--- receive_email.py
+++ receive_email.py
@@ -2,10 +2,13 @@ import logging
 import base64
 
 from google.appengine.api import mail
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 
+import model
+from util import *
 
 class ReceiveEmail(InboundMailHandler):
 
@@ -13,13 +16,21 @@ class ReceiveEmail(InboundMailHandler):
         body = '\n'.join([b.decode() for (a, b) in message.bodies(content_type='text/plain')])
         sender = message.sender
         to = message.to.split('@')[0].split('.')
-        waveId = base64.urlsafe_b64decode(to[0])
-        waveletId = base64.urlsafe_b64decode(to[1])
-        #date returns the message date.
-        #attachments is a list of element pairs containing file types and contents.
 
-        logging.debug('incoming email from %s [waveId=%s, waveletId=%s]: %s'
-                      % (sender, waveId, waveletId, body))
+        if to[0].startswith('remove-'):
+            logging.debug('unsubscribe %s' % mail_from)
+            mail_to = base64.urlsafe_b64decode(to[0][7:])
+            query = model.ParticipantPreferences.all()
+            query.filter('email =', mail_to)
+            for pp in query:
+                pp.email = ''
+                pp.put()
+            mail.send_mail(ROBOT_EMAIL, mail_to, UNSUBSCRIBED_SUBJECT, UNSUBSCRIBED)
+        else:
+            logging.debug('incoming email from %s [waveId=%s, waveletId=%s]: %s'
+                          % (sender, waveId, waveletId, body))
+            waveId = base64.urlsafe_b64decode(to[0])
+            waveletId = base64.urlsafe_b64decode(to[1])
 
 
 if __name__ == '__main__':
