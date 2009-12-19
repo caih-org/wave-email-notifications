3f4bfdafabd3d038c895d54fd53a1ff7469f1b1d
cesar <cesar@caih.org>
2009-12-19 03:39:01 -0200

    Fixes for several issues including detection of online users

--- notifiy/proc.py
+++ notifiy/proc.py
@@ -4,8 +4,10 @@
 from __future__ import absolute_import
 
 import urllib
+import datetime
 
 from google.appengine.ext import webapp
+from google.appengine.ext import deferred
 
 from . import model
 from . import preferences
@@ -20,27 +22,34 @@ class Process(webapp.RequestHandler):
         participant = urllib.unquote(path[2])
         waveId = urllib.unquote(path[3])
         notification_type = urllib.unquote(path[4])
+        toggle = (notification_type == 'toggle')
 
-        toggle = self.request.get('toggle')
-
-        pwp = preferences.get_pwp(participant, waveId, create=bool(toggle))
+        pwp = preferences.get_pwp(participant, waveId, create=toggle)
 
         if pwp:
-            if toggle:
-                pwp.notify_type = (pwp.notify_type + 1) % model.NOTIFY_TYPE_COUNT
-                pwp.put()
-            else:
-                pwp.visited = True;
-                pwp.put()
-
             if notification_type == "email":
                 self.response.out.write(str(pwp.notify_type))
-            else:
+            elif notification_type == "phone":
                 pp = preferences.get_pp(participant)
-                if pwp.notify_type != 0 and pp and pp.phone_token:
+                if pwp.notify_type != 0 and pp and len(pp.get_phone_preferences()) > 0:
                     self.response.out.write("1")
                 else:
                     self.response.out.write("0")
+            elif notification_type == "status":
+                self.response.out.write(str(pwp.notify_type))
+            elif notification_type == "online":
+                pwp.last_visited = datetime.datetime.now();
+                pwp.put()
+                deferred.defer(visited, pwp, _countdown=90);
+            elif notification_type == "toggle":
+                pwp.notify_type = (pwp.notify_type + 1) % model.NOTIFY_TYPE_COUNT
+                pwp.put()
         else:
             self.response.out.write(str(model.NOTIFY_NONE))
 
+
+def visited(pwp):
+    current_pwp = preferences.get_pwp(pwp.participant, pwp.waveId)
+    if current_pwp.last_visited == pwp.last_visited:
+        current_pwp.visited = True;
+        current_pwp.put();
